03708db90ae8314375c4eeebd84630270915fa74
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2019-09-22 17:04:05 -0400

    Add files via upload

---
+++ Lab3/insertionSort.c
@@ -0,0 +1,34 @@
+#include "mySort.h"
+#include "metrics.h"
+#include <stdio.h>
+
+
+
+
+void mySort(int array[], unsigned int first, unsigned int last)
+    {
+	if(sizeof(array)==0)
+		{
+			printf("There are no elements to sort");
+		}
+   else {
+		//int j=first;
+		int j;
+		myCopy(&first,&j);
+		for (j;j<=last;j++){
+		int k;
+		myCopy(&array[j],&k);
+			int i=j-1;
+				while ((i>=0) && (myCompare(array[i],k)>0)){
+					
+					mySwap(&array[i],&array[i+1]);
+					i--;			
+					}
+				myCopy(&k,&array[i+1]);
+		
+				
+		}
+	}
+       
+return;
+    }
---
+++ Lab3/mergeSort.c
@@ -0,0 +1,78 @@
+#include "mySort.h"
+
+#include <stdio.h>
+
+#include "metrics.h"
+
+int temp[MAX_SIZE_N_TO_SORT];
+	
+void merge(int array[], int first,int last,int tempp[]){
+
+int mid=(first+last)/2;
+
+int i;
+myCopy(&first,&i);
+
+int l=mid+1;
+
+int j;
+myCopy(&l,&j);
+
+int q;
+
+for(q=first;q<=last;q++)
+	{
+
+	if(myCompare(i,l)==0)
+		{
+		mySwap(&array[j],&tempp[q]);
+		j++;
+		}
+	else if(j==last+1)
+		{
+		mySwap(&array[i],&tempp[q]);
+		i++;
+		}
+	else if(myCompare(array[i],array[j])<0)
+		{
+		mySwap(&array[i],&tempp[q]);
+		i++;
+		}
+	else    {
+		mySwap(&array[j],&tempp[q]);
+		j++;
+		}
+	}
+int r=0;
+
+for(r=first;r<=last;r++)
+	{
+		myCopy(&tempp[r],&array[r]);
+	}
+return ;
+}
+	
+void mySort(int array[], unsigned int first, unsigned int last)
+
+ {     
+	if(first>=last){
+
+		return;
+
+		}
+ 	if(first<last){
+
+		int middle=(first+last)/2;
+
+		mySort(array,first,middle);
+
+		mySort(array,middle+1,last);
+
+		merge(array,first,last,temp);
+		
+	}
+return ;		
+}
+	
+		
+	
