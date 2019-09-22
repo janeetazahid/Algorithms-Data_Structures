bbfcb1a0348567cd44eadde3e840aa46f46c9dcb
janeetazahid <55668993+janeetazahid@users.noreply.github.com>
2019-09-22 17:29:26 -0400

    Add files via upload

---
+++ Lab6/intHeap.c
@@ -0,0 +1,145 @@
+
+/**
+ *  The functions in this module implement a Heapdata structure
+ *  of integers.
+ */
+
+
+/**
+ * heapDelete() removes the biggest integer in the heap and returns it.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+int heap[100];
+static top=0;
+void heapinitalize()//initalizes to array of 0s
+	{
+		int i; 
+		for(i=0;i<sizeof(heap);i++)
+			{
+				heap[i]=0;
+			}
+	}
+
+int heapDelete()
+{	int root;//swaps root with smallest index and returns root
+	int size=heapSize();
+	size--; 
+	root=heap[0];
+	int temp1;
+	int temp2;
+	temp1=heap[0];
+	temp2=heap[size];
+	heap[size]=temp1;
+	heap[0]=temp2;
+	top--;
+	heapify(0);//heapify from top down
+	return(root);
+}
+//seprate function
+// return root of tree
+//heapify agian make argumnet equal to index which we are starting to heapify from
+
+/**
+ *  addHeap(thing2add) adds the "thing2add" to the Heap.
+ *
+ */
+void addHeap(int thing2add)
+{
+	if(top==0)//first element
+		{
+			heap[0]=thing2add;	
+			top++;
+		}
+	else
+		{
+			int i=top;
+			if(thing2add<heap[((i+1)/2)-1])//new key doesnt cause any violations
+				{
+					heap[top]=thing2add;
+					top++;
+				}
+			if(thing2add>heap[((i+1)/2)-1])//new key causes violations check till done
+				{
+					heap[top]=thing2add;
+					top++;
+					while(thing2add>heap[((i+1)/2)-1])	
+						{
+							heap[i]=heap[((i+1)/2)-1];
+							heap[((i+1)/2)-1]=heap[i];
+							i--;
+					
+						}
+					
+				}
+		}
+}
+
+/**
+ * heapSize() returns the number of items in the Heap.
+ *
+ */
+int heapSize()
+{
+	
+	return top; 
+}
+
+void heapify (int k)
+{	
+	int rightindex=(2*k)+2;
+	int leftindex=(2*k)+1;
+	int size=heapSize();
+	int bigger=k;
+	if((rightindex<size) &&( heap[rightindex]>heap[bigger]))//right child bigger
+		{
+			bigger=rightindex;
+		}
+	if((leftindex<size)&& (heap[leftindex]>heap[bigger]) )//left child bigger 
+		{
+			bigger=leftindex;
+		}
+	if(bigger!=k)
+		{
+			int temp1;
+			int temp2;
+			temp1=heap[bigger];
+			temp2=heap[k];
+			heap[k]=temp1;
+			heap[bigger]=temp2;
+			heapify(bigger);
+		}
+	
+}
+
+void printheap(int k)
+{	
+	
+	int index;
+	index=heapSize();		
+	printf("<node id='%d'>",heap[k]);
+	if((k*2)+1<index)
+		{
+			printheap((k*2)+1);
+		}
+	if(((2*k)+2)<index)
+			{
+				printheap((2*k)+2);
+			}
+	 
+	printf("</node>");		
+		
+
+}
+
+void dectop()
+	{
+		top--;
+	}
+
+
+
+
---
+++ Lab6/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+extern int pop();
+extern void push(int);
+extern int heapDelete();
+extern void addHeap(int);
+extern int heapSize();
+extern void heapinitalize();
+extern void printheap(int);
+extern int heapSize();
+extern void dectop();
+int main(int argc, char * argv[])
+{
+ 	int value;
+	int i=0;
+	int del;
+	heapinitalize();
+ 	while (scanf("%d\n", &value) != EOF) 
+	{
+   		fprintf(stderr, "READING INPUT: %d\n", value);
+    		addHeap(value);
+		
+    		
+    	}
+	printheap(0);//good
+	printf("\n");
+	int numelements;
+	numelements=heapSize();
+	numelements--;
+	for (i=0;i<=numelements;i++)
+		{
+			del=heapDelete();
+			push(del);
+				
+		}
+   	 for(i=0;i<= numelements;i++)
+		{
+			printf("%d\n",pop());
+		}
+  	
+  exit(0);
+}
+
